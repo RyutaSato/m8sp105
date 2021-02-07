6a42a929211aa3d62076991da16f87f972a280ee
maron8saton <sato.ryuta.main@gmail.com>
2021-02-07 21:24:27 +0900

    Initial Commit

---
+++ m8sp105/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  m8sp105
+//
+//  Created by 佐藤竜太 on 2021/02/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ m8sp105/m8sp105App.swift
@@ -0,0 +1,17 @@
+//
+//  m8sp105App.swift
+//  m8sp105
+//
+//  Created by 佐藤竜太 on 2021/02/07.
+//
+
+import SwiftUI
+
+@main
+struct m8sp105App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
